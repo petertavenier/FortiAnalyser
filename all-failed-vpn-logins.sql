e92c53dd9c6a2f817be6ed1683df037392ba5152
petertavenier <petertavenier@users.noreply.github.com>
2017-02-08 22:31:33 +0100

    all-failed-vpn-logins
    this query gives you a set of all failed VPN logins

---
+++ all-failed-vpn-logins.sql
@@ -0,0 +1,15 @@
+SELECT vpn_user, tunneltype, time, remote_ip
+FROM 
+ ###(SELECT 
+  coalesce(nullifna(`xauthuser`), `user`) AS vpn_user, 
+  tunneltype,
+  from_dtime(dtime) AS time,
+  ipstr(`remip`) AS remote_ip
+ FROM $log 
+ WHERE $filter 
+  AND subtype='vpn' 
+  AND (tunneltype='ipsec' OR left(tunneltype, 3)='ssl') 
+  AND action IN ('ssl-login-fail', 'ipsec-login-fail') 
+  AND coalesce(nullifna(`xauthuser`), nullifna(`user`)) IS NOT NULL 
+ GROUP BY vpn_user, tunneltype, time, remote_ip)### t 
+GROUP BY vpn_user, tunneltype, time, remote_ip ORDER BY time DESC
