8bb5762b43dd9e8f42fe2a9714ee26043c2a403a
petertavenier <petertavenier@users.noreply.github.com>
2017-02-09 19:07:06 +0100

    Update all-failed-vpn-logins.sql
    start_time is a bit more clear

--- all-failed-vpn-logins.sql
+++ all-failed-vpn-logins.sql
@@ -1,9 +1,9 @@
-SELECT vpn_user, tunneltype, time, remote_ip
+SELECT vpn_user, tunneltype, start_time, remote_ip
 FROM 
  ###(SELECT 
   coalesce(nullifna(`xauthuser`), `user`) AS vpn_user, 
   tunneltype,
-  from_dtime(dtime) AS time,
+  from_dtime(dtime) AS start_time,
   ipstr(`remip`) AS remote_ip
  FROM $log 
  WHERE $filter 
@@ -11,5 +11,5 @@ FROM
   AND (tunneltype='ipsec' OR left(tunneltype, 3)='ssl') 
   AND action IN ('ssl-login-fail', 'ipsec-login-fail') 
   AND coalesce(nullifna(`xauthuser`), nullifna(`user`)) IS NOT NULL 
- GROUP BY vpn_user, tunneltype, time, remote_ip)### t 
-GROUP BY vpn_user, tunneltype, time, remote_ip ORDER BY time DESC
+ GROUP BY vpn_user, tunneltype, start_time, remote_ip)### t 
+GROUP BY vpn_user, tunneltype, start_time, remote_ip ORDER BY start_time DESC
